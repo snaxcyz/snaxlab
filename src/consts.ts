5a4e3d036f611183fc8eee1c78f6dcff91113c84
snaxcyz <snaxcyz@gmail.com>
2025-09-25 01:51:52 +0500

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,50 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'SnaxLab',
+  description:
+    '',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'jktrn',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/authors',
+    label: 'authors',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/snaxcyz',
+    label: 'GitHub',
+  },
+  {
+    href: 'https://x.com/mahmudjanovr',
+    label: 'Twitter',
+  },
+  {
+    href: 'snaxcyz@gmail.com',
+    label: 'Email',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+}
---
+++ src/content/authors/enscribe.md
@@ -0,0 +1,9 @@
+---
+name: 'snaxcyz'
+pronouns: 'he/him'
+avatar: 'https://gravatar.com/avatar/c40dd573a62deeb72d3c9a5e4dbc2efc?size=256'
+bio: '3791'
+twitter: 'https://x.com/mahmudjanovr'
+github: 'https://github.com/snaxcyz'
+mail: 'snaxcyz@gmail.com'
+---
